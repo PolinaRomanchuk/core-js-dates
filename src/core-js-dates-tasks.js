d7923ccc7fdc3e08ee52e54db71cad4003cc22b1
PolinaRomanchuk <polinar_99@mail.ru>
2025-03-22 19:24:00 +0300

    feat: finish isLeapYear

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -317,8 +317,16 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const newDate = new Date(date);
+  const year = newDate.getFullYear();
+  if (year % 100 === 0 && year % 400 === 0) {
+    return true;
+  }
+  if (year % 4 === 0) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = {
